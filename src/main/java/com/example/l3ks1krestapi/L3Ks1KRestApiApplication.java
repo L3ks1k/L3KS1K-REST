d0cb75229a390790a34a3c2fa3ac1cabcd624dcf
adi7312 <114097246+adi7312@users.noreply.github.com>
2024-06-20 23:18:59 +0200

    init repo

---
+++ src/main/java/com/example/l3ks1krestapi/L3Ks1KRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.l3ks1krestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L3Ks1KRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L3Ks1KRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/l3ks1krestapi/L3Ks1KRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l3ks1krestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L3Ks1KRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
